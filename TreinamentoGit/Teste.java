9c86bd990b3b379f387b173f443cd3ec3034ea8b
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-20 16:53:18 -0300

    Not Hello World

--- TreinamentoGit/Teste.java
+++ TreinamentoGit/Teste.java
@@ -7,11 +7,17 @@ public class Teste {
         for (String s : data) {
             System.out.println(s);
             System.out.println("Hello World");
-            //teste
-            //Teste2
         }
         for (String s : data){
             System.console().writer().println(s);
+        }
+    }
+
+    public static void main(String arg1){
+        if(arg1 == "hello"){
+            System.out.println(arg1 + " World!");
+        }else{
+            System.out.println("It's not a hello world!");
         }
     }
 }
